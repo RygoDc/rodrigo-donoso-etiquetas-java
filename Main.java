9908ba7902cf3166390e7ccaa81541c1006436a2
rygodc <rdc.1985@gmail.com>
2024-10-17 11:04:42 +0200

    Cambios en el main

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 public class Main{
 	public static void main (String[] args){
-		if(args.length == 0){
-			System.out.println("No hay argumentos");
-		} else {
-			System.out.println("Hay " +args.length() + " argumentos");
+	
+	try{
+		for(int i=0; i < args.legth(); i++){
+			System.out.println("Argumento " + args[i]);
 		}
+	}catch(IndexOutOfBoundsException e){
+		System.out.println("Error :" + e);
 	}
+		
 }
