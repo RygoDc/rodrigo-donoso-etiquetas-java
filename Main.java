f565478eb540a35ec890b85ccd9b395ce2a93fbe
rygodc <rdc.1985@gmail.com>
2024-10-17 10:44:07 +0200

    Impresion de argumentos en Main

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main{
 	public static void main (String[] args){
-		System.out.println("");
+		if(args.length == 0){
+			System.out.println("No hay argumentos");
+		} else {
+			System.out.println("Hay " +args.length() + " argumentos");
+		}
 	}
 }
